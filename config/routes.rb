5428a064c45dea737705aae4782a847434be55e8
Yuki <tigers24.backfrip.yuki1117@gmail.com>
2017-01-04 14:36:26 +0000

    論文投稿サイト(2) - 新規データの投稿完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'articles' => 'articles#index'
   get 'articles/new' => 'articles#new'
+  post 'articles' => 'articles#create'
   get 'articles/:id' => 'articles#show'
 end
